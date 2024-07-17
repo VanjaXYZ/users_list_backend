0d877d21e8a6dcb76834eabfe1af07286b8621de
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-18 00:41:59 +0100

    created

---
+++ src/mock_data/users_default_data.ts
@@ -0,0 +1,22 @@
+import { Users } from 'src/users/interfaces/users.interface';
+
+export const users: Users[] = [
+  {
+    id: 1,
+    name: 'Andy',
+    age: 34,
+    email: 'andy.eriksson@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'George',
+    age: 51,
+    email: 'g.wolf@yahoo.com',
+  },
+  {
+    id: 3,
+    name: 'Dick',
+    age: 22,
+    email: 'dicken2033@gmail.com',
+  },
+];
