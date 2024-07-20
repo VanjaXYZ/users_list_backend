4b6a6cb8e751d8b0083ef58cadcdaa15abfb3f8e
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 07:36:19 +0100

    add TypeORMModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'manageit2023',
+      username: 'postgres',
+      entities: [User],
+      database: process.env.DATABASE_URL,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
