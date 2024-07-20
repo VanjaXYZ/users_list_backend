aac027173e2d8e227952a304e8b8a756e92f6d67
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 07:37:00 +0100

    created

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 40 })
+  email: string;
+
+  @Column({ type: 'int' })
+  age: number;
+}
