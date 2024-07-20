68e844d139c20bcd8810ec6c00f638614dac9699
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 08:42:49 +0100

    email must be unique

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,7 +8,7 @@ export class User {
   @Column({ type: 'varchar', length: 30 })
   name: string;
 
-  @Column({ type: 'varchar', length: 40 })
+  @Column({ type: 'varchar', length: 40, unique: true })
   email: string;
 
   @Column({ type: 'int' })
