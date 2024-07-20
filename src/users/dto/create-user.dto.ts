8e12360660e6dcfccab171179ed5f0050d3590f2
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-20 07:36:55 +0100

    refactor

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,16 @@
-import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @MinLength(2, { message: 'name must have atleast 2 characters.' })
   name: string;
 
-  @IsEmail()
+  @IsEmail(null, { message: 'Please provide valid email.' })
   email: string;
 
   @IsNumber()
   age: number;
 
-  @IsNumber()
-  id: number;
+  // @IsNumber()
+  // id: number;
 }
