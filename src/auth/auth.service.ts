a5317ba4d3d744dab9732aedb3894a2870a7f34b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-23 08:40:34 +0100

    removed fakeUsers

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,6 @@ import { AuthPayloadDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
-const fakeUsers = [
-  { id: 1, username: 'anson', password: 'password' },
-  { id: 2, username: 'jack', password: 'password123' },
-];
-
 @Injectable()
 export class AuthService {
   constructor(
@@ -19,8 +14,6 @@ export class AuthService {
     username,
     password,
   }: AuthPayloadDto): Promise<string | null> {
-    // console.log('USERS: ', this.usersService);
-    // const findUser = fakeUsers.find((user) => user.username === username);
     const findUser = await this.usersService.findByUsername(username);
     if (!findUser) return null;
     if (password === findUser.password) {
