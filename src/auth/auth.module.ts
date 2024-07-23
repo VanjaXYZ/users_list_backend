93c7944c795b0271f34f80f40019d613d9a2f1d5
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-07-23 09:42:16 +0100

    add jwt to env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './strategies/local.strategy';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.register({
-      secret: 'abc123',
+      secret: `${process.env.JWT_SECRET}`,
       signOptions: { expiresIn: '1h' },
     }),
     UsersModule,
